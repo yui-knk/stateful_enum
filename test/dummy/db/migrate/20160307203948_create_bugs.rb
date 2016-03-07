ba7f7ef3a071cecdf289b42bed4224ea1ecac114
Akira Matsuda <ronnie@dio.jp>
2016-03-08 05:40:13 +0900

    rails g model bug title dscription status:integer assigned_to:integer resolved_at:datetime

---
+++ test/dummy/db/migrate/20160307203948_create_bugs.rb
@@ -0,0 +1,13 @@
+class CreateBugs < ActiveRecord::Migration
+  def change
+    create_table :bugs do |t|
+      t.string :title
+      t.string :dscription
+      t.integer :status
+      t.integer :assigned_to
+      t.datetime :resolved_at
+
+      t.timestamps null: false
+    end
+  end
+end
