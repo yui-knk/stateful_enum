368c9f27a17e8d528f140b4ef880fe6a4f1900ad
Akira Matsuda <ronnie@dio.jp>
2016-03-08 08:43:54 +0900

    Shut up 'n play yer migrations

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@
 ENV["RAILS_ENV"] = "test"
 
 require File.expand_path("../../test/dummy/config/environment.rb",  __FILE__)
+ActiveRecord::Migration.verbose = false
 ActiveRecord::Migrator.migrate (ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../test/dummy/db/migrate", __FILE__)])
 
 require "rails/test_help"
