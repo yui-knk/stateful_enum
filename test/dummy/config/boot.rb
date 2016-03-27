92bab98240819f5ccfc8bf33b02cc43d87eebdb4
Akira Matsuda <ronnie@dio.jp>
2016-03-28 06:32:08 +0900

    Let BUNDLE_GEMFILE be absolute
    in order to invoke another app process inside the test directory for the generator test

--- test/dummy/config/boot.rb
+++ test/dummy/config/boot.rb
@@ -1,5 +1,6 @@
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../../../Gemfile', __FILE__)
+ENV['BUNDLE_GEMFILE'] = File.expand_path(ENV['BUNDLE_GEMFILE'], Dir.pwd)
 
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
 $LOAD_PATH.unshift File.expand_path('../../../../lib', __FILE__)
