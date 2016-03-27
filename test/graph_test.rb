336c0359e8eeef86ed170707da0c5a501823aace
Akira Matsuda <ronnie@dio.jp>
2016-03-28 06:49:45 +0900

    Test something is generated

---
+++ test/graph_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class GraphTest < ActiveSupport::TestCase
+  def test_graph
+    FileUtils.rm_f Rails.root.join('Bug.png')
+
+    Dir.chdir Rails.root do
+      `rails g stateful_enum:graph bug`
+    end
+
+    assert File.exist?(Rails.root.join('Bug.png'))
+  end
+end
