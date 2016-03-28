a49aad27a93c514eaa1afafa2bb499dcedde406a
Akira Matsuda <ronnie@dio.jp>
2016-03-29 05:37:13 +0900

    :golf:

--- lib/generators/stateful_enum/graph_generator.rb
+++ lib/generators/stateful_enum/graph_generator.rb
@@ -32,7 +32,7 @@ module StatefulEnum
 
         instance_eval(&block)
 
-        (@g.each_edge.map {|e| e.node_two }.uniq - @g.each_edge.map {|e| e.node_one }.uniq).each do |final|
+        (@g.each_edge.map(&:node_two).uniq - @g.each_edge.map(&:node_one).uniq).each do |final|
           @g.get_node(final) {|n| n['shape'] = 'doublecircle' }
         end
 
