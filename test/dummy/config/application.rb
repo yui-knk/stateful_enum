920369d72e8f6fae2a18fd099f144e5d31a12b70
Akira Matsuda <ronnie@dio.jp>
2016-03-28 09:46:37 +0900

    Rails 4.1.config.AR doesn't respond_to raise_in_transactional_callbacks

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -20,7 +20,6 @@ module Dummy
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true if Rails::VERSION::STRING < '5'
+    config.active_record.raise_in_transactional_callbacks = true if Rails::VERSION::STRING =~ /^4.2/
   end
 end
-
